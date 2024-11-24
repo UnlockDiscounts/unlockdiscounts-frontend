5bcb3b236cbf0ddcc9b063c64280d4b3e1e4f582
Anshpatel2434 <anshpatel2434@gmail.com>
2024-11-24 18:36:33 +0530

    Resolved searched products error in deployment

--- src/components/SearchedProductsPage.js
+++ src/components/SearchedProductsPage.js
@@ -11,6 +11,7 @@ const SearchedProductsPage = () => {
 	const { searched_param } = useParams();
 	const { products } = useProducts();
 	const [allData, setAllData] = useState([]);
+	const [filteredData, setFilteredData] = useState([]);
 
 	useEffect(() => {
 		if (products) {
@@ -18,6 +19,10 @@ const SearchedProductsPage = () => {
 			applyFilters(searched_param);
 		}
 	}, [products]);
+
+	useEffect(() => {
+		applyFilters(searched_param);
+	}, [allData]);
 
 	const applyFilters = (searchQuery = "") => {
 		if (allData.length > 0) {
@@ -32,7 +37,7 @@ const SearchedProductsPage = () => {
 					product.section.toLowerCase().includes(searchQuery.toLowerCase());
 				return matchesSearch;
 			});
-			setAllData(filtered);
+			setFilteredData(filtered);
 		}
 	};
 
@@ -43,14 +48,14 @@ const SearchedProductsPage = () => {
 				<div className="header-container">
 					<div className="text-container">
 						<div className="description">
-							{allData.length} result for "{searched_param}"
+							{filteredData.length} result for "{searched_param}"
 						</div>
 					</div>
 				</div>
 				<div className="sp_separater"></div>
 				<div className="search_product-page-cards-container">
-					{allData.length > 0 ? (
-						allData.map((item, index) => (
+					{filteredData.length > 0 ? (
+						filteredData.map((item, index) => (
 							<ProductPageCard data={item} key={index} />
 						))
 					) : (
