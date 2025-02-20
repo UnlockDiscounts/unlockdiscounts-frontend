11a967a0b236254925fca9794536fe6e345beec9
ShubhamPhogat <sphogat444@gmail.com>
2025-02-20 18:55:35 +0530

    updated height and width of the search button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,48 +8,48 @@ import "../styles/Navbar.css";
 import Logo from "../assets/Logo.svg";
 
 const Navbar = () => {
-	const [showSidebar, setShowSidebar] = useState(false);
-	const [showDropdown, setShowDropdown] = useState(false);
-	const navigate = useNavigate();
-	const dropdownRef = useRef(null);
-	const [searchQuery, setSearchQuery] = useState("");
-	const [sidebarsearchQuery, setSideBarSearchQuery] = useState("");
-	const [homePage, setHomePage] = useState(true);
+  const [showSidebar, setShowSidebar] = useState(false);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const navigate = useNavigate();
+  const dropdownRef = useRef(null);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sidebarsearchQuery, setSideBarSearchQuery] = useState("");
+  const [homePage, setHomePage] = useState(true);
 
-	const categories = [
-		{ name: "Electronics", path: "products/Electronics" },
-		{ name: "Online Learning", path: "online_learning" },
-		{ name: "Banking", path: "banking" },
-	];
+  const categories = [
+    { name: "Electronics", path: "products/Electronics" },
+    { name: "Online Learning", path: "online_learning" },
+    { name: "Banking", path: "banking" },
+  ];
 
-	// Close dropdown when clicking outside
-	useEffect(() => {
-		const handleClickOutside = (event) => {
-			if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-				setShowDropdown(false);
-			}
-		};
+  // Close dropdown when clicking outside
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setShowDropdown(false);
+      }
+    };
 
-		document.addEventListener("mousedown", handleClickOutside);
-		return () => document.removeEventListener("mousedown", handleClickOutside);
-	}, []);
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
 
-	const handleNavigate = (category) => {
-		navigate(`/${category}`);
-		window.location.reload();
-		setShowDropdown(false);
-		setShowSidebar(false);
-	};
+  const handleNavigate = (category) => {
+    navigate(`/${category}`);
+    window.location.reload();
+    setShowDropdown(false);
+    setShowSidebar(false);
+  };
 
-	return (
-		<div className="navbar-container">
-		<div className="navbar">
-			<div className="navbar-logo" onClick={() => navigate("/")}>
-				<img src={Logo} alt="Logo" />
-			</div>
+  return (
+    <div className="navbar-container">
+      <div className="navbar">
+        <div className="navbar-logo" onClick={() => navigate("/")}>
+          <img src={Logo} alt="Logo" />
+        </div>
 
-			{/* Categories Dropdown */}
-			{/* <div className="navbar-fonts relative" ref={dropdownRef}>
+        {/* Categories Dropdown */}
+        {/* <div className="navbar-fonts relative" ref={dropdownRef}>
 				<div
 					className="flex items-center cursor-pointer"
 					onClick={() => setShowDropdown(!showDropdown)}
@@ -72,71 +72,77 @@ const Navbar = () => {
 				)}
 			</div> */}
 
-<p
-				className="navbar-fonts"
-				onClick={() => handleNavigate("products/Mens")}
-				// setHomePage={setHomePage(true)}
-			>
-				Men
-			</p>
-			<p
-				className="navbar-fonts"
-				onClick={() => handleNavigate("products/Women")}
-			>
-				Women
-			</p>
-			<p
-				className="navbar-fonts"
-				onClick={() => handleNavigate("products/Kids")}
-			>
-				Kids
-			</p>
+        <p
+          className="navbar-fonts"
+          onClick={() => handleNavigate("products/Mens")}
+          // setHomePage={setHomePage(true)}
+        >
+          Men
+        </p>
+        <p
+          className="navbar-fonts"
+          onClick={() => handleNavigate("products/Women")}
+        >
+          Women
+        </p>
+        <p
+          className="navbar-fonts"
+          onClick={() => handleNavigate("products/Kids")}
+        >
+          Kids
+        </p>
 
-			<p
-				className="navbar-fonts"
-				onClick={() => {handleNavigate("products/Electronics")
-				setHomePage(false)}}
-			>
-				Electronics
-			</p>
-			<p
-				className="navbar-fonts"
-				onClick={() => handleNavigate("online_learning")}
-				// setHomePage={setHomePage(true)}
-			>
-				Online Learning
-			</p>
-			<p
-				className="navbar-fonts"
-				onClick={() => handleNavigate("products/Banking")}
-				// setHomePage={setHomePage(true)}
-			>
-				Banking
-			</p>
+        <p
+          className="navbar-fonts"
+          onClick={() => {
+            handleNavigate("products/Electronics");
+            setHomePage(false);
+          }}
+        >
+          Electronics
+        </p>
+        <p
+          className="navbar-fonts"
+          onClick={() => handleNavigate("online_learning")}
+          // setHomePage={setHomePage(true)}
+        >
+          Online Learning
+        </p>
+        <p
+          className="navbar-fonts"
+          onClick={() => handleNavigate("products/Banking")}
+          // setHomePage={setHomePage(true)}
+        >
+          Banking
+        </p>
 
-			<div className="navbar-search-container">
-				<input
-					className="navbar-search-input"
-					placeholder="Search products ..."
-					value={searchQuery}
-					onChange={(e) => {
-						setSearchQuery(e.target.value);
-					}}
-					onKeyDown={(e) => {
-						if (e.key === "Enter" && searchQuery.trim()) {
-							navigate(`/searched_products/${searchQuery}`);
-							window.location.reload();
-						}
-					}}
-					
-				/>
-					<CiSearch className="navbar-search-icon"
-						onClick={(e)=>{
-							if(searchQuery !== ""){
-							navigate(`/searched_products/${searchQuery}/${sidebarsearchQuery}`);
-							window.location.reload();
-						}}}/>
-				{/* <button
+        <div className="navbar-search-container">
+          <input
+            className="navbar-Search-Input"
+            placeholder="Search products ..."
+            value={searchQuery}
+            onChange={(e) => {
+              setSearchQuery(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && searchQuery.trim()) {
+                navigate(`/searched_products/${searchQuery}`);
+                window.location.reload();
+              }
+            }}
+          />
+          <CiSearch
+            className="navbar-search-icon"
+            onClick={(e) => {
+              if (searchQuery !== "") {
+                navigate(
+                  `/searched_products/${searchQuery}/${sidebarsearchQuery}`
+                );
+                window.location.reload();
+              }
+            }}
+          />
+          {/* <button
 					className="navbar_search_button"
 					onClick={() => {
 						navigate(`/searched_products/${searchQuery}`);
@@ -145,85 +151,91 @@ const Navbar = () => {
 						>
 						Search
 						</button> */}
-			</div>
+        </div>
 
-			<div className="navbar-login" onClick={(e)=>{navigate("/signup")}}>
-				<p className="navbar-login-text">Sign Up / Login</p>
-			</div>
+        <div
+          className="navbar-login"
+          onClick={(e) => {
+            navigate("/signup");
+          }}
+        >
+          <p className="navbar-login-text">Sign Up / Login</p>
+        </div>
 
-			<RiMenu4Fill
-				className="navbar-mobile-popup"
-				onClick={() => {
-					setShowSidebar(true);
-					setHomePage(false);
-				}}
-			/>
+        <RiMenu4Fill
+          className="navbar-mobile-popup"
+          onClick={() => {
+            setShowSidebar(true);
+            setHomePage(false);
+          }}
+        />
 
-				{/* Sidebar Modal */}
-			{!homePage&&<div
-				className={`sidebar-overlay ${showSidebar ? "show" : ""}`}
-				onClick={() => setShowSidebar(false)}
-			>
-				<h1>Hellooooo</h1>
-				<div className="sidebar" onClick={(e) => e.stopPropagation()}>
-					<div className="sidebar-header">
-					<div className="navbar-logo" onClick={() => navigate("/")}>
-							<img src={Logo} alt="Logo" />
-						</div>
-						<IoMdClose
-							className="close-icon"
-							onClick={() => setShowSidebar(false)}
-						/>
-						
-					</div>
+        {/* Sidebar Modal */}
+        {!homePage && (
+          <div
+            className={`sidebar-overlay ${showSidebar ? "show" : ""}`}
+            onClick={() => setShowSidebar(false)}
+          >
+            <h1>Hellooooo</h1>
+            <div className="sidebar" onClick={(e) => e.stopPropagation()}>
+              <div className="sidebar-header">
+                <div className="navbar-logo" onClick={() => navigate("/")}>
+                  <img src={Logo} alt="Logo" />
+                </div>
+                <IoMdClose
+                  className="close-icon"
+                  onClick={() => setShowSidebar(false)}
+                />
+              </div>
 
-					<div className="sidebar-nav">
-						<p
-							className="sidebar-nav-item"
-							onClick={() => handleNavigate("products/Mens")}
-						>
-							Mens
-						</p>
-						<p
-							className="sidebar-nav-item"
-							onClick={() => handleNavigate("products/Women")}
-						>
-							Women
-						</p>
-						<p
-							className="sidebar-nav-item"
-							onClick={() => handleNavigate("products/Kids")}
-						>
-							Kids
-						</p>
-						<p
-							className="sidebar-nav-item"
-							onClick={() => handleNavigate("products/Electronics")}
-						>
-							Electronics
-						</p>
-						<p
-							className="sidebar-nav-item"
-							onClick={() => handleNavigate("online_learning")}
-						>
-							Online Learning
-						</p>
-						<p
-							className="sidebar-nav-item"
-							onClick={() => handleNavigate("products/Banking")}
-						>
-							Banking
-						</p>
-					</div>
+              <div className="sidebar-nav">
+                <p
+                  className="sidebar-nav-item"
+                  onClick={() => handleNavigate("products/Mens")}
+                >
+                  Mens
+                </p>
+                <p
+                  className="sidebar-nav-item"
+                  onClick={() => handleNavigate("products/Women")}
+                >
+                  Women
+                </p>
+                <p
+                  className="sidebar-nav-item"
+                  onClick={() => handleNavigate("products/Kids")}
+                >
+                  Kids
+                </p>
+                <p
+                  className="sidebar-nav-item"
+                  onClick={() => handleNavigate("products/Electronics")}
+                >
+                  Electronics
+                </p>
+                <p
+                  className="sidebar-nav-item"
+                  onClick={() => handleNavigate("online_learning")}
+                >
+                  Online Learning
+                </p>
+                <p
+                  className="sidebar-nav-item"
+                  onClick={() => handleNavigate("products/Banking")}
+                >
+                  Banking
+                </p>
+              </div>
 
-					<div className="sidebar-login">
-						<p className="sidebar-login-text">Sign up / Login</p>
-					</div>
-				</div>
-			</div>}
-		</div>
-		</div>
-	);
+              <div className="sidebar-login">
+                <p className="sidebar-login-text">Sign up / Login</p>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
